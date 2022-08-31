75bfe342427e0eca0420cc3f5b34eea7d969e316
rejalejale <79826693+rejalejale@users.noreply.github.com>
2022-08-31 11:29:47 +0700

    suskes

--- routes/web.php
+++ routes/web.php
@@ -52,3 +52,7 @@ Route::group(['middleware' => ['isAdmin','auth'],'prefix' => 'admin', 'as' => 'a
 
 Auth::routes();
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
