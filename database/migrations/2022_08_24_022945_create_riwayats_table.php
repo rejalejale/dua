981c17ce985dca5c746f558965c4101ccf3c06d4
rejalejale <79826693+rejalejale@users.noreply.github.com>
2022-09-01 13:50:49 +0700

    asd

--- database/migrations/2022_08_24_022945_create_riwayats_table.php
+++ database/migrations/2022_08_24_022945_create_riwayats_table.php
@@ -25,8 +25,8 @@ class CreateRiwayatsTable extends Migration
             $table->text('tujuan')->nullable();
             $table->text('bidang')->nullable();
             $table->text('status')->default('Inactive');
-            $table->foreignIdFor(supir::class)->constrained()->cascadeOnDelete()->nullable()->default('');
-            $table->foreignIdFor(mobil::class)->constrained()->cascadeOnDelete()->nullable()->default('');
+            $table->foreignIdFor(supir::class)->constrained()->cascadeOnDelete()->nullable();
+            $table->foreignIdFor(mobil::class)->constrained()->cascadeOnDelete()->nullable();
             
             $table->timestamps();
             $table->softDeletes();
