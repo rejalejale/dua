38cd4a2f9a7a2c5379a40b192c9f34cec5f763b4
rejalejale <79826693+rejalejale@users.noreply.github.com>
2022-09-01 15:33:44 +0700

    Update show.blade.php

--- resources/views/admin/bookings/show.blade.php
+++ resources/views/admin/bookings/show.blade.php
@@ -52,7 +52,7 @@
                             <th>Status</th>
                             <td>{{ $booking->status }}     
                                 <a href="{{ route('admin.countries.edit', $booking->id) }}" class="btn btn-info">
-                                        <i></i> Update Status
+                                         Update Status
                                 </a>     
                             </td>
                             
