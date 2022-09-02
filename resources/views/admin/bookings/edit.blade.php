4704e330fdea0e0dfae736b0dd49f88e519b3db2
rejalejale <79826693+rejalejale@users.noreply.github.com>
2022-09-02 09:40:40 +0700

    Update edit.blade.php

--- resources/views/admin/bookings/edit.blade.php
+++ resources/views/admin/bookings/edit.blade.php
@@ -84,8 +84,17 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.20.1/moment.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
     <script>
-        $('.datetimepicker').datetimepicker({
-            format: "YYYY-MM-DD HH:mm"
+         $('.datetimepicker').datetimepicker({
+            format: 'YYYY-MM-DD HH:mm',
+            locale: 'en',
+            sideBySide: true,
+            icons: {
+            up: 'fas fa-chevron-up',
+            down: 'fas fa-chevron-down',
+            previous: 'fas fa-chevron-left',
+            next: 'fas fa-chevron-right'
+            },
+            stepping: 10
         });
     </script>
 @endpush
